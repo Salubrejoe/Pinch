c4fb6f4be3aca9cc050b679014a7879fec192f69
Lorenzo Polato <44895517+Salubrejoe@users.noreply.github.com>
2023-03-08 09:57:48 +0000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Lore P on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
